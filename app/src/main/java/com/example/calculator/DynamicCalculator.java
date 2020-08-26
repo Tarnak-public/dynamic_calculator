fed8b2a574ad065655ac2a35ea949b38c3b70fae
Tarnak-public <krzys.kretus@gmail.com>
2020-08-26 12:24:28 +0200

    guide

--- app/src/main/java/com/example/calculator/DynamicCalculator.java
+++ app/src/main/java/com/example/calculator/DynamicCalculator.java
@@ -2,6 +2,10 @@ package com.example.calculator;
 
 public class DynamicCalculator {
 
+    /* how to guide:
+        https://medium.com/@kalpeshchandora12/dynamic-code-loading-in-android-dea83ba3bc85
+     */
+
     public static double add(double d1, double d2) {
         return d1 + d2;
     }
